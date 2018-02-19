4891eb152ab907eac5221ce3f7eca441595fbe3a
kavinandhu1001 <30829042+kavinandhu1001@users.noreply.github.com>
2018-02-19 16:26:08 +0530

    Create 101.c

---
+++ 101.c
@@ -0,0 +1,21 @@
+
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+char a[10],b[10];
+int i,n;
+scanf("%d",&n);
+printf("enter the string");
+gets(a);
+b=strrev(a);
+for(i=0;i<n;i++)
+{
+    if(i==n)
+    {
+        printf("%c,b[i]);
+        break;
+    }
+    
+}
+}
