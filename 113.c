ebd540a3bf43327a317794663a036dfe98265353
kavinandhu1001 <30829042+kavinandhu1001@users.noreply.github.com>
2018-02-28 16:30:00 +0530

    Update 113.c

--- 113.c
+++ 113.c
@@ -1,14 +1,25 @@
-#include <stdio.h>
-int main(void) {
-	int a[3],i,k,count=0;
-	scanf("%d",&k);
-	for(i=0;i<3;i++)
-	scanf("%d",&a[i]);
-	for(i=0;i<3;i++)
-	{
-		if(a[i]==k)
-		++count;
-	}
-	printf("%d times",count);
-	return 0;
+
+
+
+#include<stdio.h>
+int main()
+{
+int n,i,k,count=0,a[10];
+scanf("%d",&n);
+scanf("%d",&k);
+for(i=0;i<n;i++)
+{
+    scanf("%d",&a[i]);
+}
+
+for(i=0;i<n;i++)
+{
+if(a[i]==k)
+{
+++count;
+
+}}
+    printf("%d",count);
+    return 0;
+
 }
